59419b6d33dc59abfeb1afbc610c1a3bceb8fb1f
Justin Bender <jrkbender@gmail.com>
2020-05-11 13:28:01 -0400

    Angular frontend for the node stream

---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { environment } from '../environments/environment';
+declare const flvjs: any;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'scott-stream';
+
+  ngOnInit() {
+    this.setupFlv();
+  }
+
+  setupFlv() {
+    if (flvjs.isSupported()) {
+      const videoElement = document.getElementById('videoElement');
+      const flvPlayer = flvjs.createPlayer({
+          type: 'flv',
+          url: `http://${environment.IPADDR}:4300/live/${environment.STREAMCODE}.flv`
+      });
+      flvPlayer.attachMediaElement(videoElement);
+      flvPlayer.load();
+      flvPlayer.play();
+    }
+  }
+}
